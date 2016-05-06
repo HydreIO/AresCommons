1a0e8808db78ba67af5698e468cf9ceed8f1da4b
Sceat <contact@aresrpg.fr>
2016-05-06 18:21:12 +0200

    remove log

--- src/main/java/fr/aresrpg/commons/log/Logger.java
+++ src/main/java/fr/aresrpg/commons/log/Logger.java
@@ -1,26 +1,5 @@
 package fr.aresrpg.commons.log;
 
-public class Logger implements Log {
-
-	private static Logger logger = new Logger();
-
-	@Override
-	public void logOut(String log) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void logTrace(Exception e) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void logTrace(Throwable t) {
-		// TODO Auto-generated method stub
-	}
-
-	public static Logger getLogger() {
-		return logger;
-	}
+public class Logger {
 
 }
--- src/main/java/fr/aresrpg/commons/util/concurrent/Threads.java
+++ src/main/java/fr/aresrpg/commons/util/concurrent/Threads.java
@@ -2,15 +2,13 @@ package fr.aresrpg.commons.util.concurrent;
 
 import java.util.concurrent.TimeUnit;
 
-import fr.aresrpg.commons.log.Log;
-
 public final class Threads {
 
 	public static void sleep(int i, TimeUnit unit) {
 		try {
 			Thread.sleep(unit.toMillis(i));
 		} catch (InterruptedException e) {
-			Log.trace(e);
+			e.printStackTrace();
 		}
 	}
 
