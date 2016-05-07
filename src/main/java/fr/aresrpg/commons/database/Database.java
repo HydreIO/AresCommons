f8b7dd6174edebc3063e09b6351459602b251c5e
Duarte David <deltaduartedavid@gmail.com>
2016-05-08 00:26:17 +0200

    Little fix on database

--- src/main/java/fr/aresrpg/commons/database/Database.java
+++ src/main/java/fr/aresrpg/commons/database/Database.java
@@ -1,19 +1,26 @@
 package fr.aresrpg.commons.database;
 
+import fr.aresrpg.commons.Iterators;
 import fr.aresrpg.commons.Value;
 
 import java.io.Closeable;
+import java.util.Iterator;
 
-public interface Database<T> extends Closeable , Value<Collection<T>> {
+public interface Database extends Closeable , Value<Collection> {
 
     void connect(String host , int port , String user , String password);
 
-    void drop(Collection<T> collection);
+    void drop(Collection<?> collection);
 
-    Collection<T> create(String id);
+    <T> Collection<T> create(String id , Class<T> clazz);
 
-    Collection<T>[] getCollections();
+    Collection[] getCollections();
 
-    Collection<T> get(String id);
+    <T> Collection<T> get(String id);
+
+    @Override
+    default Iterator<Collection> iterator(){
+        return Iterators.of(getCollections());
+    }
 
 }
