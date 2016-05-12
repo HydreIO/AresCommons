69883d9b9de906eaa10f29206ef869671bbd1638
Sceat <contact@aresrpg.fr>
2016-05-12 17:49:07 +0200

    add Collecotrs and few things

--- src/main/java/fr/aresrpg/commons/util/collection/HashSet.java
+++ src/main/java/fr/aresrpg/commons/util/collection/HashSet.java
@@ -1,5 +1,13 @@
 package fr.aresrpg.commons.util.collection;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class HashSet<E> extends java.util.HashSet<E> implements Set<E> {
 
 	/**
@@ -7,4 +15,136 @@ public class HashSet<E> extends java.util.HashSet<E> implements Set<E> {
 	 */
 	private static final long serialVersionUID = 2249609191019766975L;
 
+	public static class UnmodifiableSet<E> extends UnmodifiableCollection<E> implements java.util.Set<E>, Serializable {
+
+		public UnmodifiableSet(java.util.Set<? extends E> c) {
+			super(c);
+		}
+
+		private static final long serialVersionUID = -9215047833775013803L;
+
+		public boolean equals(Object o) {
+			return o == this || c.equals(o);
+		}
+
+		public int hashCode() {
+			return c.hashCode();
+		}
+	}
+
+	static class UnmodifiableCollection<E> implements Collection<E>, Serializable {
+		private static final long serialVersionUID = 1820017752578914078L;
+
+		final Collection<? extends E> c;
+
+		UnmodifiableCollection(Collection<? extends E> c) {
+			if (c == null) throw new NullPointerException();
+			this.c = c;
+		}
+
+		public int size() {
+			return c.size();
+		}
+
+		public boolean isEmpty() {
+			return c.isEmpty();
+		}
+
+		public boolean contains(Object o) {
+			return c.contains(o);
+		}
+
+		public Object[] toArray() {
+			return c.toArray();
+		}
+
+		public <T> T[] toArray(T[] a) {
+			return c.toArray(a);
+		}
+
+		public String toString() {
+			return c.toString();
+		}
+
+		public Iterator<E> iterator() {
+			return new Iterator<E>() {
+				private final Iterator<? extends E> i = c.iterator();
+
+				public boolean hasNext() {
+					return i.hasNext();
+				}
+
+				public E next() {
+					return i.next();
+				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void forEachRemaining(Consumer<? super E> action) {
+					// Use backing collection version
+					i.forEachRemaining(action);
+				}
+			};
+		}
+
+		public boolean add(E e) {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean remove(Object o) {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean containsAll(Collection<?> coll) {
+			return c.containsAll(coll);
+		}
+
+		public boolean addAll(Collection<? extends E> coll) {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean removeAll(Collection<?> coll) {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean retainAll(Collection<?> coll) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void clear() {
+			throw new UnsupportedOperationException();
+		}
+
+		// Override default methods in Collection
+		@Override
+		public void forEach(Consumer<? super E> action) {
+			c.forEach(action);
+		}
+
+		@Override
+		public boolean removeIf(Predicate<? super E> filter) {
+			throw new UnsupportedOperationException();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Spliterator<E> spliterator() {
+			return (Spliterator<E>) c.spliterator();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Stream<E> stream() {
+			return (Stream<E>) c.stream();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Stream<E> parallelStream() {
+			return (Stream<E>) c.parallelStream();
+		}
+	}
 }
---
+++ src/main/java/fr/aresrpg/commons/util/stream/Collectors.java
@@ -0,0 +1,74 @@
+package fr.aresrpg.commons.util.stream;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class Collectors {
+
+	static final Set<Collector.Characteristics> CH_UNORDERED_ID = new fr.aresrpg.commons.util.collection.HashSet.UnmodifiableSet<>(EnumSet.of(Collector.Characteristics.UNORDERED,
+			Collector.Characteristics.IDENTITY_FINISH));
+
+	public static <T> Collector<T, ?, fr.aresrpg.commons.util.collection.Set<T>> toSet() {
+		return new CollectorImpl<>((Supplier<Set<T>>) HashSet::new, Set<T>::add, (left, right) -> {
+			left.addAll(right);
+			return left;
+		}, CH_UNORDERED_ID);
+	}
+
+	public static class CollectorImpl<T, A, R> implements Collector<T, A, R> {
+		private final Supplier<A> supplier;
+		private final BiConsumer<A, T> accumulator;
+		private final BinaryOperator<A> combiner;
+		private final Function<A, R> finisher;
+		private final Set<Characteristics> characteristics;
+
+		public CollectorImpl(Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Function<A, R> finisher, Set<Characteristics> characteristics) {
+			this.supplier = supplier;
+			this.accumulator = accumulator;
+			this.combiner = combiner;
+			this.finisher = finisher;
+			this.characteristics = characteristics;
+		}
+
+		public CollectorImpl(Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Set<Characteristics> characteristics) {
+			this(supplier, accumulator, combiner, castingIdentity(), characteristics);
+		}
+
+		@SuppressWarnings("unchecked")
+		private static <I, R> Function<I, R> castingIdentity() {
+			return i -> (R) i;
+		}
+
+		@Override
+		public BiConsumer<A, T> accumulator() {
+			return accumulator;
+		}
+
+		@Override
+		public Supplier<A> supplier() {
+			return supplier;
+		}
+
+		@Override
+		public BinaryOperator<A> combiner() {
+			return combiner;
+		}
+
+		@Override
+		public Function<A, R> finisher() {
+			return finisher;
+		}
+
+		@Override
+		public Set<Characteristics> characteristics() {
+			return characteristics;
+		}
+	}
+
+}
