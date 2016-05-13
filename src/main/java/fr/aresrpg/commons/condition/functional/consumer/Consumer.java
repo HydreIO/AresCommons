e6a37cea7c34b06b39d4e75c35b4a67ae4ffa85f
Sceat <contact@aresrpg.fr>
2016-05-13 10:26:06 +0200

    Remove the ambiguous aldebaran Function split the two function of Aldebaran :
    	- chain with the same arg -> refactored with Consumer's class
    enhancement
    
    	- direct execution -> ExecutiveConsumer (accessed with
    Consumers.execute(args)

--- src/main/java/fr/aresrpg/commons/condition/functional/consumer/Consumer.java
+++ src/main/java/fr/aresrpg/commons/condition/functional/consumer/Consumer.java
@@ -1,19 +1,35 @@
 package fr.aresrpg.commons.condition.functional.consumer;
 
 @FunctionalInterface
-public interface Consumer<A> {
-    void accept(A a );
+public interface Consumer<T> {
+	void accept(T a);
 
-    default Consumer<A> then(Consumer<A> after){
-        return a -> {
-            accept(a);
-            after.accept(a);
-        };
-    }
+	default Consumer<T> then(Consumer<T> w) {
+		return (T t) -> {
+			accept(t);
+			w.accept(t);
+		};
+	}
 
-    static <A> Consumer<A> empty(){
-        return o ->{
-            //Empty
-        };
-    }
+	static <T> Consumer<T> empty() {
+		return t -> {};
+	}
+
+	default <A, B> BiConsumer<A, B> both(Consumer<A> wa, Consumer<B> wb) {
+		return (A a, B b) -> {
+			wa.accept(a);
+			wb.accept(b);
+		};
+	}
+
+	default java.util.function.Consumer<T> toNative() {
+		return this::accept;
+	}
+
+	default <A, B> java.util.function.BiConsumer<A, B> toNativeBoth(Consumer<A> wa, Consumer<B> wb) {
+		return (A a, B b) -> {
+			wa.accept(a);
+			wb.accept(b);
+		};
+	}
 }
---
+++ src/main/java/fr/aresrpg/commons/condition/functional/consumer/ExecutiveConsumer.java
@@ -0,0 +1,27 @@
+package fr.aresrpg.commons.condition.functional.consumer;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+@FunctionalInterface
+public interface ExecutiveConsumer<T> {
+
+	void accept(T t);
+
+	default <U> ExecutiveConsumer<T> then(ExecutiveConsumer<U> other, U u) {
+		other.accept(u);
+		return none();
+	}
+
+	default <U> ExecutiveConsumer<T> thenNative(java.util.function.Consumer<U> other, U u) {
+		other.accept(u);
+		return none();
+	}
+
+	static <T> ExecutiveConsumer<T> none() {
+		return Empty.EMPTY;
+	}
+
+	class Empty {
+		private static final ExecutiveConsumer EMPTY = (T) -> {};
+	}
+
+}
