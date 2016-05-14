fe2988fc902147a5782c4610f755e70fded725ce
Duarte David <deltaduartedavid@gmail.com>
2016-05-14 13:04:56 +0200

    Rename package

--- src/main/java/fr/aresrpg/commons/serialization/annotations/SerializedName.java
+++ src/main/java/fr/aresrpg/commons/serialization/annotations/SerializedName.java
@@ -1,4 +1,4 @@
-package fr.aresrpg.commons.serialization.annotation;
+package fr.aresrpg.commons.serialization.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
