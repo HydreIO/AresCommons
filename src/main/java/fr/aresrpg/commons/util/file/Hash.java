31765b6f56c12c9e222fa9baed9aa49f17bb35e2
Sceat <contact@aresrpg.fr>
2016-05-15 08:51:49 +0200

    hash util

---
+++ src/main/java/fr/aresrpg/commons/util/file/Hash.java
@@ -0,0 +1,41 @@
+package fr.aresrpg.commons.util.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+import fr.aresrpg.commons.log.Logger;
+
+public enum Hash {
+
+	MD5("MD5"),
+	SHA1("SHA1"),
+	SHA256("SHA-256"),
+	SHA512("SHA-512");
+
+	private String name;
+
+	Hash(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public byte[] checksum(File input) {
+		try (InputStream in = new FileInputStream(input)) {
+			MessageDigest digest = MessageDigest.getInstance(getName());
+			byte[] block = new byte[4096];
+			int length;
+			while ((length = in.read(block)) > 0)
+				digest.update(block, 0, length);
+			return digest.digest();
+		} catch (Exception e) {
+			Logger.trace(e);
+		}
+		return new byte[0];
+	}
+
+}
