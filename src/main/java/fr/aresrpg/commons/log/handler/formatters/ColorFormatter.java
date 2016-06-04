aaa95a564b32871b970539b1c7419adc5e5383cf
Sceat <mr.sceat@outlook.com>
2016-06-04 20:24:32 +0200

    fail commit

--- src/main/java/fr/aresrpg/commons/log/handler/formatters/ColorFormatter.java
+++ src/main/java/fr/aresrpg/commons/log/handler/formatters/ColorFormatter.java
@@ -4,30 +4,30 @@ import fr.aresrpg.commons.log.AnsiColors;
 import fr.aresrpg.commons.log.Log;
 import fr.aresrpg.commons.log.Logger;
 
-public class ColorFormatter implements Formatter{
+public class ColorFormatter implements Formatter {
 	private Formatter parent;
 
 	public ColorFormatter(Formatter parent) {
 		this.parent = parent;
 	}
 
-	public String getColor(Logger.Level level){
+	public String getColor(Logger.Level level) {
 		String color;
-		switch (level){
+		switch (level) {
 			case INFO:
-				color = AnsiColors.getCode(AnsiColors.AnsiColor.YELLOW , null , false);
+				color = AnsiColors.getCode(AnsiColors.AnsiColor.YELLOW, null, false);
 				break;
 			case DEBUG:
-				color = AnsiColors.getCode(AnsiColors.AnsiColor.PURPLE , null , false);
+				color = AnsiColors.getCode(AnsiColors.AnsiColor.PURPLE, null, false);
 				break;
 			case ERROR:
-				color = AnsiColors.getCode(AnsiColors.AnsiColor.RED , null , false);
+				color = AnsiColors.getCode(AnsiColors.AnsiColor.RED, null, false);
 				break;
 			case SEVERE:
-				color = AnsiColors.getCode(AnsiColors.AnsiColor.RED , null , true);
+				color = AnsiColors.getCode(AnsiColors.AnsiColor.RED, null, true);
 				break;
 			case SUCCESS:
-				color = AnsiColors.getCode(AnsiColors.AnsiColor.GREEN , null , false);
+				color = AnsiColors.getCode(AnsiColors.AnsiColor.GREEN, null, false);
 				break;
 			default:
 				color = "";
@@ -38,6 +38,6 @@ public class ColorFormatter implements Formatter{
 
 	@Override
 	public String format(Log log, ErrorFormatter errorFormatter) {
-		return getColor(log.getLevel()) + parent.format(log , errorFormatter) + AnsiColors.ANSI_RESET;
+		return getColor(log.getLevel()) + parent.format(log, errorFormatter) + AnsiColors.ANSI_RESET;
 	}
 }
