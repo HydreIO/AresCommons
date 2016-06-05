ba6bee8d5cbc02d0e1d83494cd17093068336006
Sceat <mr.sceat@outlook.com>
2016-06-05 15:16:32 +0200

    add temporary errorFormater :santa_tone5:

--- src/main/java/fr/aresrpg/commons/domain/log/handler/BaseHandler.java
+++ src/main/java/fr/aresrpg/commons/domain/log/handler/BaseHandler.java
@@ -1,12 +1,24 @@
 package fr.aresrpg.commons.domain.log.handler;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import fr.aresrpg.commons.domain.log.Log;
 import fr.aresrpg.commons.domain.log.handler.formatters.ErrorFormatter;
 import fr.aresrpg.commons.domain.log.handler.formatters.Formatter;
 
 public abstract class BaseHandler implements Handler {
 	private Formatter formatter;
-	private ErrorFormatter errorFormatter = (e , l) -> "";
+	// private ErrorFormatter errorFormatter = (e , l) -> "";
+
+	private ErrorFormatter errorFormatter = (e, l) -> {
+		if (l == null) return "[" + e.name() + "] Exception without cause just throwed !";
+		StringWriter sw = new StringWriter(1024);
+		final PrintWriter pw = new PrintWriter(sw);
+		l.printStackTrace(pw);
+		pw.flush();
+		return "[" + e.name() + "] " + sw.toString();
+	};
 
 	public void setFormatter(Formatter formatter) {
 		this.formatter = formatter;
@@ -16,7 +28,6 @@ public abstract class BaseHandler implements Handler {
 		this.errorFormatter = errorFormatter;
 	}
 
-
 	public Formatter getFormatter() {
 		return formatter;
 	}
@@ -25,7 +36,8 @@ public abstract class BaseHandler implements Handler {
 		return errorFormatter;
 	}
 
-	protected String format(Log log){
-		return formatter.format(log , errorFormatter);
+	protected String format(Log log) {
+		return formatter.format(log, errorFormatter);
 	}
+
 }
