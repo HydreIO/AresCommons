cf17002b472139f1be7f3a78c1bc6eb3d873d935
Sceat <mr.sceat@outlook.com>
2016-07-27 01:17:29 +0200

    add var in default configured value

--- src/main/java/fr/aresrpg/commons/domain/config/Configured.java
+++ src/main/java/fr/aresrpg/commons/domain/config/Configured.java
@@ -3,7 +3,8 @@ package fr.aresrpg.commons.domain.config;
 public @interface Configured {
 	/**
 	 * The path , this path can be separated
+	 * 
 	 * @return the path in the configuration
 	 */
-	String value();
+	String value() default "var.";
 }
