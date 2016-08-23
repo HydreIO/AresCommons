e34aba98807422ba7142727ad29a611cbad9e091
Sceat <mr.sceat@outlook.com>
2016-08-23 02:45:42 +0200

    fix conversion string error

--- src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
+++ src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
@@ -1,32 +1,35 @@
 package fr.aresrpg.commons.domain.util.schedule;
 
-import java.util.Arrays;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
-import fr.aresrpg.commons.domain.concurrent.ThreadBuilder;
+import fr.aresrpg.commons.domain.concurrent.Pool;
+import fr.aresrpg.commons.domain.concurrent.Pool.PoolType;
+import fr.aresrpg.commons.domain.condition.Option;
 import fr.aresrpg.commons.domain.log.Logger;
+
+import java.util.Arrays;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * The class to launch {@link Scheduled} instance
+ * 
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public class Scheduler {
 
 	private static Scheduler instance = new Scheduler();
 
-	private ScheduledExecutorService pool = Executors.newScheduledThreadPool(50, new ThreadBuilder().setName("Scheduler pool - [Thrd: %1$]").toFactory());
+	private ScheduledExecutorService pool = Pool.builder().setName("Scheduler pool - [Thrd: $1]").setType(PoolType.SCHEDULED).toScheduled(Option.of(50));
 
-	private Scheduler() {}
+	private Scheduler() {
+	}
 
 	/**
 	 * Get the scheduler instance
+	 * 
 	 * @return the scheduler instance
 	 */
 	public static Scheduler getScheduler() {
 		return instance;
 	}
-
 
 	/**
 	 * Shutdown the scheduler , if this method is not called the application will not exit
@@ -36,8 +39,10 @@ public class Scheduler {
 	}
 
 	/**
-	 * Register the  method annotated with {@link Schedule} in {@link Scheduled} instance for scheduling
-	 * @param scheduled the scheduled instance to register
+	 * Register the method annotated with {@link Schedule} in {@link Scheduled} instance for scheduling
+	 * 
+	 * @param scheduled
+	 *            the scheduled instance to register
 	 */
 	public void register(Scheduled scheduled) {
 		Arrays.stream(scheduled.getClass().getMethods()).filter(m -> m.isAnnotationPresent(Schedule.class)).forEach(m -> {
@@ -47,7 +52,7 @@ public class Scheduler {
 				try {
 					m.invoke(scheduled, new Object[m.getParameterCount()]);
 				} catch (Exception e) {
-					Logger.MAIN_LOGGER.debug(e , "[Scheduler] Error with " + "//: " + m.getDeclaringClass() + "//:" + m.getName());
+					Logger.MAIN_LOGGER.debug(e, "[Scheduler] Error with " + "//: " + m.getDeclaringClass() + "//:" + m.getName());
 				}
 			}, ns, ns, java.util.concurrent.TimeUnit.NANOSECONDS);
 		});
