3473ae76d5dfd6c9b3ae30a9e87c1a3261ab1390
Sceat <mr.sceat@outlook.com>
2016-08-25 17:06:46 +0200

    update database
    remove the createCollection method because "get" must create the collection if she doesn't exist

--- src/main/java/fr/aresrpg/commons/domain/database/Database.java
+++ src/main/java/fr/aresrpg/commons/domain/database/Database.java
@@ -1,44 +1,52 @@
 package fr.aresrpg.commons.domain.database;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public interface Database extends Closeable {
 
 	/**
 	 * Connect to database
-	 * @param host host to connect
-	 * @param port the port to use
-	 * @param user the user to use
-	 * @param password the password to use
+	 * 
+	 * @param host
+	 *            host to connect
+	 * @param port
+	 *            the port to use
+	 * @param user
+	 *            the user to use
+	 * @param password
+	 *            the password to use
+	 * @throws IOException
+	 *             if the database isn't reachable for whatever reason
 	 */
-	void connect(String host, int port, String user, String password);
+	void connect(String host, int port, String user, String password) throws IOException;
 
 	/**
 	 * Delete the provided collection
-	 * @param collection the collection to delete
+	 * 
+	 * @param collection
+	 *            the collection to delete
 	 */
 	void drop(Collection<?> collection);
 
 	/**
-	 * Create a collection
-	 * @param id the name of the new collection
-	 * @param clazz the type of the documents in collection
-	 * @param <T> the type of the collection
-	 * @return the created collection
-	 */
-	<T> Collection<T> create(String id, Class<T> clazz);
-
-	/**
 	 * Get the collections in the database
+	 * 
 	 * @return the collections in the database
 	 */
 	Collection<?>[] getCollections();
 
 	/**
-	 * Get the a collection using the provided id
-	 * @param id the name of the collection
-	 * @param clazz the type of the documents in collection
-	 * @param <T> the type of the collection
+	 * <p>
+	 * Get the a collection using the provided id and create it if there is no collection for the id
+	 * </p>
+	 * 
+	 * @param id
+	 *            the name of the collection
+	 * @param clazz
+	 *            the type of the documents in collection
+	 * @param <T>
+	 *            the type of the collection
 	 * @return the collection
 	 */
 	<T> Collection<T> get(String id, Class<T> clazz);
