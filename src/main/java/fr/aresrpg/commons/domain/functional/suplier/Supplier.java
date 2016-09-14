c308a775c31fbd64183ad6354aeaac1934584c48
Sceat <mr.sceat@outlook.com>
2016-09-14 15:40:34 +0200

    adding a way to retrieve a java native supplier with a ares supplier

--- src/main/java/fr/aresrpg/commons/domain/functional/suplier/Supplier.java
+++ src/main/java/fr/aresrpg/commons/domain/functional/suplier/Supplier.java
@@ -1,7 +1,11 @@
 package fr.aresrpg.commons.domain.functional.suplier;
 
 @FunctionalInterface
-public interface Supplier<T> extends TrySupplier<T>{
+public interface Supplier<T> extends TrySupplier<T> {
 	@Override
 	T get();
+
+	default java.util.function.Supplier<T> toNative() {
+		return () -> get();
+	}
 }
