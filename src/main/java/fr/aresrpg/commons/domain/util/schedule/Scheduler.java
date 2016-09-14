48836cebc6081812301435cf567bb4bc13c7bb88
Sceat <mr.sceat@outlook.com>
2016-09-14 20:14:34 +0200

    update the scheduler

--- src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
+++ src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
@@ -5,6 +5,7 @@ import fr.aresrpg.commons.domain.log.Logger;
 
 import java.util.Arrays;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The class to launch {@link Scheduled} instance
@@ -15,6 +16,7 @@ import java.util.concurrent.ScheduledExecutorService;
 public class Scheduler {
 
 	private ScheduledExecutorService pool;
+	private int taskCount;
 
 	public Scheduler(ScheduledExecutorService pool) {
 		this.pool = pool;
@@ -25,6 +27,15 @@ public class Scheduler {
 	}
 
 	/**
+	 * The task count represent the number of method or runnable registered in the scheduler
+	 * 
+	 * @return the taskCount
+	 */
+	public int getTaskCount() {
+		return taskCount;
+	}
+
+	/**
 	 * Shutdown the scheduler , if this method is not called the application will not exit
 	 */
 	public void shutdown() {
@@ -32,15 +43,64 @@ public class Scheduler {
 	}
 
 	/**
-	 * Register the method annotated with {@link Schedule} in {@link Scheduled} instance for scheduling
+	 * Register a runnable which will be executed every X nano
+	 * 
+	 * @param runnable
+	 *            the runnable
+	 * @param nano
+	 *            the interval time in nano second
+	 */
+	public void register(Runnable runnable, long nano) {
+		taskCount++;
+		pool.scheduleAtFixedRate(runnable, nano, nano, TimeUnit.NANOSECONDS);
+	}
+
+	/**
+	 * Register a runnable which will be executed every X time
+	 * 
+	 * @param runnable
+	 *            the runnable
+	 * @param time
+	 *            the interval time
+	 * @param unit
+	 *            the time unit of the interval time
+	 */
+	public void register(Runnable runnable, long time, TimeUnit unit) {
+		register(runnable, unit.toNanos(time));
+	}
+
+	/**
+	 * Register all the method with the {@linkplain Schedule} annotation from a {@linkplain Scheduled} class
 	 * 
 	 * @param scheduled
-	 *            the scheduled instance to register
+	 *            the class instance
 	 */
 	public void register(Scheduled scheduled) {
 		Arrays.stream(scheduled.getClass().getMethods()).filter(m -> m.isAnnotationPresent(Schedule.class)).forEach(m -> {
 			Schedule s = m.getAnnotation(Schedule.class);
+			register(() -> {
+				try {
+					m.invoke(scheduled, new Object[m.getParameterCount()]);
+				} catch (Exception e) {
+					// TODO @DeltaEvo
+				}
+			} , s.unit().toNanos(s.rate()));
+		});
+	}
+
+	/**
+	 * Register the method annotated with {@link Schedule} in {@link Scheduled} instance for scheduling
+	 * 
+	 * @param scheduled
+	 *            the scheduled instance to register
+	 * @deprecated
+	 */
+	@Deprecated
+	public void registerOld(Scheduled scheduled) {
+		Arrays.stream(scheduled.getClass().getMethods()).filter(m -> m.isAnnotationPresent(Schedule.class)).forEach(m -> {
+			Schedule s = m.getAnnotation(Schedule.class);
 			long ns = s.unit().toNanos(s.rate());
+			taskCount++;
 			pool.scheduleAtFixedRate(() -> {
 				try {
 					m.invoke(scheduled, new Object[m.getParameterCount()]);
