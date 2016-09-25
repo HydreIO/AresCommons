3a0a5d1282799da28819e3dd986b4f8bbd1b5cae
Sceat <mr.sceat@outlook.com>
2016-09-25 23:29:47 +0200

    Doc on SerializationFactory

--- src/main/java/fr/aresrpg/commons/domain/serialization/factory/SerializationFactory.java
+++ src/main/java/fr/aresrpg/commons/domain/serialization/factory/SerializationFactory.java
@@ -8,20 +8,28 @@ import fr.aresrpg.commons.domain.serialization.field.FieldModifier;
 import java.util.List;
 
 /**
- * An serialization factory that create {@link Serializer}
- * @param <I> the input type
- * @param <O> the output type
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * A serialization factory that create {@link Serializer}
+ * 
+ * @param <I>
+ *            the input type
+ * @param <O>
+ *            the output type
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
-public interface SerializationFactory<I , O> {
-	<T> Serializer<T , I , O> createSerializer(Class<T> clazz);
-	<T> Serializer<T , I , O> createOrGetSerializer(Class<T> clazz);
+public interface SerializationFactory<I, O> {
+	<T> Serializer<T, I, O> createSerializer(Class<T> clazz);
 
-	List<Adapter<? , ?>> getAdapters();
-	void addAdapter(Adapter<? , ?> adapter);
-	void removeAdapter(Adapter<? , ?> adapter);
-	<T> Adapter<T , ?> getAdapter(ParametrizedClass<T> clazz);
+	<T> Serializer<T, I, O> createOrGetSerializer(Class<T> clazz);
+
+	List<Adapter<?, ?>> getAdapters();
+
+	void addAdapter(Adapter<?, ?> adapter);
+
+	void removeAdapter(Adapter<?, ?> adapter);
+
+	<T> Adapter<T, ?> getAdapter(ParametrizedClass<T> clazz);
 
 	FieldModifier getFieldModifier();
+
 	void setFieldModifier(FieldModifier modifier);
 }
