415c92e0c68cb1560b43329c7e5aa492aaa60bae
Sceat <mr.sceat@outlook.com>
2016-09-25 23:29:36 +0200

    Doc on serializedName

--- src/main/java/fr/aresrpg/commons/domain/serialization/annotations/SerializedName.java
+++ src/main/java/fr/aresrpg/commons/domain/serialization/annotations/SerializedName.java
@@ -1,20 +1,27 @@
 package fr.aresrpg.commons.domain.serialization.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
- * Set the serialized name of the field
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * A serialized name is used to provide a generic name to a field after his serialization.
+ * </p>
+ * For example in a database you may want to use a custom field name instead of the native one
+ * 
+ * <pre>
+ * &#64;SerializedName("userId")
+ * private long id;
+ * </pre>
+ * 
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
+ * @author Sceat {@literal <sceat@aresrpg.fr>}
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface SerializedName {
-    /**
-     * Name in the collection of this field
-     * @return name in the collection of this field
-     */
-    String value();
+	/**
+	 * Name in the collection of this field
+	 * 
+	 * @return name in the collection of this field
+	 */
+	String value();
 }
