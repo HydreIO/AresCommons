cdac21eabcabd1ea5ae5f83aec84028660e078ac
Sceat <mr.sceat@outlook.com>
2016-09-25 16:16:14 +0200

    Doc on scheduled

--- src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduled.java
+++ src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduled.java
@@ -2,6 +2,7 @@ package fr.aresrpg.commons.domain.util.schedule;
 
 /**
  * Must be implemented by all class containing {@link Schedule} annotation
+ * 
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public interface Scheduled {}
