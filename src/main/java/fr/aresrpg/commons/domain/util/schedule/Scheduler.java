bfb83209bedb5d72bb8982907f226516a0ba6c9a
Sceat <mr.sceat@outlook.com>
2016-09-25 16:16:23 +0200

    Doc on scheduler

--- src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
+++ src/main/java/fr/aresrpg/commons/domain/util/schedule/Scheduler.java
@@ -1,7 +1,6 @@
 package fr.aresrpg.commons.domain.util.schedule;
 
 import fr.aresrpg.commons.domain.concurrent.ThreadPoolBuilder;
-import fr.aresrpg.commons.domain.log.Logger;
 
 import java.util.Arrays;
 import java.util.concurrent.ScheduledExecutorService;
@@ -85,29 +84,6 @@ public class Scheduler {
 					// TODO @DeltaEvo
 				}
 			} , s.unit().toNanos(s.rate()));
-		});
-	}
-
-	/**
-	 * Register the method annotated with {@link Schedule} in {@link Scheduled} instance for scheduling
-	 * 
-	 * @param scheduled
-	 *            the scheduled instance to register
-	 * @deprecated
-	 */
-	@Deprecated
-	public void registerOld(Scheduled scheduled) {
-		Arrays.stream(scheduled.getClass().getMethods()).filter(m -> m.isAnnotationPresent(Schedule.class)).forEach(m -> {
-			Schedule s = m.getAnnotation(Schedule.class);
-			long ns = s.unit().toNanos(s.rate());
-			taskCount++;
-			pool.scheduleAtFixedRate(() -> {
-				try {
-					m.invoke(scheduled, new Object[m.getParameterCount()]);
-				} catch (Exception e) {
-					Logger.MAIN_LOGGER.debug(e, "[Scheduler] Error with " + "//: " + m.getDeclaringClass() + "//:" + m.getName());
-				}
-			} , ns, ns, java.util.concurrent.TimeUnit.NANOSECONDS);
 		});
 	}
 
