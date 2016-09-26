a3741ea952f9bfa33885e17205aacb9f896b3624
Sceat <mr.sceat@outlook.com>
2016-09-26 02:20:08 +0200

    Doc on FieldNamer

--- src/main/java/fr/aresrpg/commons/domain/serialization/field/FieldNamer.java
+++ src/main/java/fr/aresrpg/commons/domain/serialization/field/FieldNamer.java
@@ -2,13 +2,16 @@ package fr.aresrpg.commons.domain.serialization.field;
 
 /**
  * A field name
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * 
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 @FunctionalInterface
 public interface FieldNamer {
 	/**
 	 * Transform the name of the field
-	 * @param name the source name
+	 * 
+	 * @param name
+	 *            the source name
 	 * @return a transformed name
 	 */
 	String transform(String name);
