26dc15f1b73a45a85796ba5f3d98ead4837d4b4d
Sceat <mr.sceat@outlook.com>
2016-09-26 02:36:03 +0200

    Doc on StreamHandler

--- src/main/java/fr/aresrpg/commons/domain/log/handler/StreamHandler.java
+++ src/main/java/fr/aresrpg/commons/domain/log/handler/StreamHandler.java
@@ -5,10 +5,10 @@ import fr.aresrpg.commons.domain.log.Log;
 import java.io.IOException;
 import java.io.OutputStream;
 
-
 /**
  * An handler using streams
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * 
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public class StreamHandler extends BaseHandler {
 	private OutputStream outStream;
@@ -17,9 +17,13 @@ public class StreamHandler extends BaseHandler {
 
 	/**
 	 * Create a stream handler using passed streams and charset
-	 * @param outStream the output stream stdout
-	 * @param errorStream the error stream stderr
-	 * @param charset the charset
+	 * 
+	 * @param outStream
+	 *            the output stream stdout
+	 * @param errorStream
+	 *            the error stream stderr
+	 * @param charset
+	 *            the charset
 	 */
 	public StreamHandler(OutputStream outStream, OutputStream errorStream, String charset) {
 		this.outStream = outStream;
@@ -29,8 +33,11 @@ public class StreamHandler extends BaseHandler {
 
 	/**
 	 * Create a stream handler using passed streams and charset UTF-8
-	 * @param outStream the output stream stdout
-	 * @param errorStream the error stream stderr
+	 * 
+	 * @param outStream
+	 *            the output stream stdout
+	 * @param errorStream
+	 *            the error stream stderr
 	 */
 	public StreamHandler(OutputStream outStream, OutputStream errorStream) {
 		this(outStream, errorStream, "UTF-8");
@@ -38,7 +45,9 @@ public class StreamHandler extends BaseHandler {
 
 	/**
 	 * Create a stream handler using passed stream
-	 * @param stream the stream to use
+	 * 
+	 * @param stream
+	 *            the stream to use
 	 */
 	public StreamHandler(OutputStream stream) {
 		this(stream, stream);
