fa4c201ed988d9aeb45f8c0cccc3fff826cd3ed6
Sceat <mr.sceat@outlook.com>
2016-09-26 02:20:46 +0200

    Doc on Serializer

--- src/main/java/fr/aresrpg/commons/domain/serialization/Serializer.java
+++ src/main/java/fr/aresrpg/commons/domain/serialization/Serializer.java
@@ -1,41 +1,56 @@
 package fr.aresrpg.commons.domain.serialization;
 
+import fr.aresrpg.commons.domain.serialization.formats.Format;
+
 import java.io.IOException;
 import java.util.Map;
 
-import fr.aresrpg.commons.domain.serialization.formats.Format;
-
 /**
- * An object mapper and serializer , deserializer
- * @param <T> the type of object to map
- * @param <I> the input type
- * @param <O> the output type
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * An object mapper, serializer and deserializer
+ * 
+ * @param <T>
+ *            the type of object to map
+ * @param <I>
+ *            the input type
+ * @param <O>
+ *            the output type
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public interface Serializer<T, I, O> {
 	/**
-	 * Map the object and serialize using the passed format and write the result to the output
-	 * @param output output to write the result
-	 * @param object the object to serialize
-	 * @param format the format to use in serialization
-	 * @throws IOException if an exception occurred
+	 * Map the object, serialize it using the passed format and write the result to the output
+	 * 
+	 * @param output
+	 *            output to write the result
+	 * @param object
+	 *            the object to serialize
+	 * @param format
+	 *            the format to use in serialization
+	 * @throws IOException
+	 *             if an exception occurred
 	 */
 	void serialize(O output, T object, Format<?, O> format) throws IOException;
 
 	/**
-	 * Map the object and deserialize using the passed format from the input
-	 * @param input input
-	 * @param format the format to use in deserialization
+	 * Map the object and deserialize it using the passed format from the input
+	 * 
+	 * @param input
+	 *            input
+	 * @param format
+	 *            the format to use in deserialization
 	 * @return the mapped object
-	 * @throws IOException if an exception occurred
+	 * @throws IOException
+	 *             if an exception occurred
 	 */
 	T deserialize(I input, Format<I, ?> format) throws IOException;
 
 	/**
 	 * Map the object passed in argument
-	 * @param values the values to map
+	 * 
+	 * @param values
+	 *            the values to map
 	 * @return the mapped object
 	 * @throws IOException
 	 */
-	T deserialize(Map<String , Object> values) throws IOException;
+	T deserialize(Map<String, Object> values) throws IOException;
 }
