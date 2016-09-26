315b663f915f0dba3c21b54a0deb9b15ad88bc4a
Sceat <mr.sceat@outlook.com>
2016-09-26 02:33:28 +0200

    Doc on I18N

--- src/main/java/fr/aresrpg/commons/domain/i18n/I18N.java
+++ src/main/java/fr/aresrpg/commons/domain/i18n/I18N.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 /**
  * A class to deal with localization
+ * 
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 @FunctionalInterface
@@ -13,29 +14,39 @@ public interface I18N {
 	/**
 	 * A class to hold the implementation
 	 */
-	class Impl{
+	class Impl {
 		private static final I18N IMPLEMENTATION = new ASMPropertiesI18N();
-		private Impl(){}
+
+		private Impl() {
+		}
 	}
 
 	/**
 	 * Create or get a new L10N bundle for the specified locale from the bundle class
-	 * @param locale the locale to create bundle
-	 * @param clazz the class of the bundle
-	 * @param <T> the type of the class
+	 * 
+	 * @param locale
+	 *            the locale to create bundle
+	 * @param clazz
+	 *            the class of the bundle
+	 * @param <T>
+	 *            the type of the class
 	 * @return a instance of the class generated with the specified locale
 	 */
 	<T extends L10N> T createL10N(Locale locale, Class<T> clazz);
 
 	/**
 	 * Create or get a new L10N bundle for the specified locale from the bundle class
-	 * @param locale the locale to create bundle
-	 * @param clazz the class of the bundle
-	 * @param <T> the type of the class
+	 * 
+	 * @param locale
+	 *            the locale to create bundle
+	 * @param clazz
+	 *            the class of the bundle
+	 * @param <T>
+	 *            the type of the class
 	 * @return a instance of the class with the specified locale
 	 */
 	static <T extends L10N> T getL10N(Locale locale, Class<T> clazz) {
-		return Impl.IMPLEMENTATION.createL10N(locale , clazz);
+		return Impl.IMPLEMENTATION.createL10N(locale, clazz);
 	}
 
 }
