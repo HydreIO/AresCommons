dde852a03eaaf6e3b72673a0accd23f0367dbe18
Sceat <mr.sceat@outlook.com>
2016-09-26 02:49:00 +0200

    Doc on Event

--- src/main/java/fr/aresrpg/commons/domain/event/Event.java
+++ src/main/java/fr/aresrpg/commons/domain/event/Event.java
@@ -1,24 +1,27 @@
 package fr.aresrpg.commons.domain.event;
 
 /**
- * A event that can be send in a {@link EventBus}
+ * A event that can be sent in a {@link EventBus}
+ * 
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public interface Event<T extends Event<T>> {
 	/**
 	 * Get the bus of this event
+	 * 
 	 * @return the bus
 	 */
 	EventBus<T> getBus();
 
 	/**
-	 * Get if this event must be call asynchronously
+	 * An asynchronous event will automatically be called on another thread
+	 * 
 	 * @return if the event is asynchronous
 	 */
 	boolean isAsynchronous();
 
 	/**
-	 * Send this event to the bus
+	 * Send this event to his bus
 	 */
 	@SuppressWarnings("unchecked")
 	default void send() {
