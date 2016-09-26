81a7a2e26c9c1b30c3713fcbbd309b61e4fe1dc9
Sceat <mr.sceat@outlook.com>
2016-09-26 02:20:00 +0200

    Doc on FieldModifier

--- src/main/java/fr/aresrpg/commons/domain/serialization/field/FieldModifier.java
+++ src/main/java/fr/aresrpg/commons/domain/serialization/field/FieldModifier.java
@@ -4,35 +4,46 @@ import java.lang.reflect.Field;
 
 /**
  * A field modifier
- * @author Duarte David  {@literal <deltaduartedavid@gmail.com>}
+ * 
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 public interface FieldModifier {
 
 	/**
 	 * Set value of the field
-	 * @param field the field to set value
-	 * @param instance the instance of the field's owner
-	 * @param value the value to set
+	 * 
+	 * @param field
+	 *            the field to set value
+	 * @param instance
+	 *            the instance of the field's owner
+	 * @param value
+	 *            the value to set
 	 */
-	void setValue(Field field , Object instance , Object value);
+	void setValue(Field field, Object instance, Object value);
 
 	/**
 	 * Get the value of a field
-	 * @param field the field to get value
-	 * @param instance the instance of the field's owner
+	 * 
+	 * @param field
+	 *            the field to get value
+	 * @param instance
+	 *            the instance of the field's owner
 	 * @return the value of a field
 	 */
-	Object getValue(Field field , Object instance);
+	Object getValue(Field field, Object instance);
 
 	/**
 	 * Test if this field can be processed
-	 * @return if it's field can be processed
+	 * 
+	 * @return true if the field can be processed
 	 */
 	boolean canProcess(Field f);
 
 	/**
 	 * Preprocess a field
-	 * @param field the field to preprocess
+	 * 
+	 * @param field
+	 *            the field to preprocess
 	 */
 	void preprocess(Field field);
 }
