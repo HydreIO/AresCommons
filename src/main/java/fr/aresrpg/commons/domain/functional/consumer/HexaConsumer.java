2ac3995e6a10508e855965f99aab3b08e5468ecf
Sceat <mr.sceat@outlook.com>
2016-09-26 02:50:42 +0200

    Doc on Hexac Penta Tetra and TriConsumer

--- src/main/java/fr/aresrpg/commons/domain/functional/consumer/HexaConsumer.java
+++ src/main/java/fr/aresrpg/commons/domain/functional/consumer/HexaConsumer.java
@@ -2,29 +2,46 @@ package fr.aresrpg.commons.domain.functional.consumer;
 
 /**
  * A consumer with 6 arguments
- * @param <A> the first argument type
- * @param <B> the second argument type
- * @param <C> the third argument type
- * @param <D> the fourth argument type
- * @param <E> the fifth argument type
- * @param <F> the sixth argument type
+ * 
+ * @param <A>
+ *            the first argument type
+ * @param <B>
+ *            the second argument type
+ * @param <C>
+ *            the third argument type
+ * @param <D>
+ *            the fourth argument type
+ * @param <E>
+ *            the fifth argument type
+ * @param <F>
+ *            the sixth argument type
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
+ * @author Sceat {@literal <sceat@aresrpg.fr>}
  */
 public interface HexaConsumer<A, B, C, D, E, F> {
 	/**
 	 * Execute an action
-	 * @param a the first argument
-	 * @param b the second argument
-	 * @param c the third argument
-	 * @param d the fourth argument
-	 * @param e the fifth argument
-	 * @param f the sixth argument
+	 * 
+	 * @param a
+	 *            the first argument
+	 * @param b
+	 *            the second argument
+	 * @param c
+	 *            the third argument
+	 * @param d
+	 *            the fourth argument
+	 * @param e
+	 *            the fifth argument
+	 * @param f
+	 *            the sixth argument
 	 */
 	void accept(A a, B b, C c, D d, E e, F f);
 
 	/**
-	 * Create an new consumer that execute this consumer an after an other
-	 * @param after the consumer to execute after
+	 * Create an new consumer which will be executed after the current one
+	 * 
+	 * @param after
+	 *            the consumer to execute after
 	 * @return a new consumer
 	 */
 	default HexaConsumer<A, B, C, D, E, F> then(HexaConsumer<A, B, C, D, E, F> after) {
--- src/main/java/fr/aresrpg/commons/domain/functional/consumer/TriConsumer.java
+++ src/main/java/fr/aresrpg/commons/domain/functional/consumer/TriConsumer.java
@@ -2,30 +2,40 @@ package fr.aresrpg.commons.domain.functional.consumer;
 
 /**
  * A consumer with 3 arguments
- * @param <A> the first argument type
- * @param <B> the second argument type
- * @param <C> the third argument type
+ * 
+ * @param <A>
+ *            the first argument type
+ * @param <B>
+ *            the second argument type
+ * @param <C>
+ *            the third argument type
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
  */
 @FunctionalInterface
-public interface TriConsumer<A , B , C> {
-    /**
-     * Execute an action
-     * @param a the first argument
-     * @param b the second argument
-     * @param c the third argument
-     */
-    void accept(A a , B b , C c);
+public interface TriConsumer<A, B, C> {
+	/**
+	 * Execute an action
+	 * 
+	 * @param a
+	 *            the first argument
+	 * @param b
+	 *            the second argument
+	 * @param c
+	 *            the third argument
+	 */
+	void accept(A a, B b, C c);
 
-    /**
-     * Create an new consumer that execute this consumer an after an other
-     * @param after the consumer to execute after
-     * @return a new consumer
-     */
-    default TriConsumer<A , B , C> then(TriConsumer<A , B , C> after){
-        return (a , b , c) -> {
-            accept(a , b , c);
-            after.accept(a , b , c);
-        };
-    }
+	/**
+	 * Create an new consumer which will be executed after the current one
+	 * 
+	 * @param after
+	 *            the consumer to execute after
+	 * @return a new consumer
+	 */
+	default TriConsumer<A, B, C> then(TriConsumer<A, B, C> after) {
+		return (a, b, c) -> {
+			accept(a, b, c);
+			after.accept(a, b, c);
+		};
+	}
 }
