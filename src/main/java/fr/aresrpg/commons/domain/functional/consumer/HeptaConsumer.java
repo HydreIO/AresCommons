a5297fd6f29c22831ba9af62dbfcefbdd353e3da
Sceat <mr.sceat@outlook.com>
2016-09-26 02:50:21 +0200

    Doc on HeptaConsumer

--- src/main/java/fr/aresrpg/commons/domain/functional/consumer/HeptaConsumer.java
+++ src/main/java/fr/aresrpg/commons/domain/functional/consumer/HeptaConsumer.java
@@ -2,32 +2,51 @@ package fr.aresrpg.commons.domain.functional.consumer;
 
 /**
  * A consumer with 7 arguments
- * @param <A> the first argument type
- * @param <B> the second argument type
- * @param <C> the third argument type
- * @param <D> the fourth argument type
- * @param <E> the fifth argument type
- * @param <F> the sixth argument type
- * @param <G> the seventh argument type
+ * 
+ * @param <A>
+ *            the first argument type
+ * @param <B>
+ *            the second argument type
+ * @param <C>
+ *            the third argument type
+ * @param <D>
+ *            the fourth argument type
+ * @param <E>
+ *            the fifth argument type
+ * @param <F>
+ *            the sixth argument type
+ * @param <G>
+ *            the seventh argument type
  * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
+ * @author Sceat {@literal <sceat@aresrpg.fr>}
  */
 @FunctionalInterface
 public interface HeptaConsumer<A, B, C, D, E, F, G> {
 	/**
 	 * Execute an action
-	 * @param a the first argument
-	 * @param b the second argument
-	 * @param c the third argument
-	 * @param d the fourth argument
-	 * @param e the fifth argument
-	 * @param f the sixth argument
-	 * @param g the seventh argument
+	 * 
+	 * @param a
+	 *            the first argument
+	 * @param b
+	 *            the second argument
+	 * @param c
+	 *            the third argument
+	 * @param d
+	 *            the fourth argument
+	 * @param e
+	 *            the fifth argument
+	 * @param f
+	 *            the sixth argument
+	 * @param g
+	 *            the seventh argument
 	 */
 	void accept(A a, B b, C c, D d, E e, F f, G g);
 
 	/**
-	 * Create an new consumer that execute this consumer an after an other
-	 * @param after the consumer to execute after
+	 * Create an new consumer which will be executed after the current one
+	 * 
+	 * @param after
+	 *            the consumer to execute after
 	 * @return a new consumer
 	 */
 	default HeptaConsumer<A, B, C, D, E, F, G> then(HeptaConsumer<A, B, C, D, E, F, G> after) {
