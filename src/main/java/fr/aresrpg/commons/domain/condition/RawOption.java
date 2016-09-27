1ed8a7d621d85506e15a3df632ac617a267f660d
Sceat <mr.sceat@outlook.com>
2016-09-27 23:58:49 +0200

    Fix inversed error in option
    Le nobel de l'érreur ultra difficile à trouver quand on débug est décernée à @DeltaEvo (heureusement que je suis puissant car putain pour aller chercher jusque dans le code des options pour trouver une erreur omg

--- src/main/java/fr/aresrpg/commons/domain/condition/RawOption.java
+++ src/main/java/fr/aresrpg/commons/domain/condition/RawOption.java
@@ -57,7 +57,7 @@ public interface RawOption<T, O extends RawOption<T, ?>> extends Value<T>, Suppl
 	 * @return the value or the wrapped value
 	 */
 	default T when(boolean condition, T value) {
-		return condition ? get() : value;
+		return condition ? value : get();
 	}
 
 	/**
